b263af0337face4f35e804a79a591625637506f0
Mário Guimarães <mario.luis.guimaraes@gmail.com>
2020-10-01 18:01:44 +0100

    Added several funs

--- lib/f2.ex
+++ lib/f2.ex
@@ -1,4 +1,9 @@
 defmodule B do
   def foo, do: :foo
+  def bar, do: :bar
+  def xpto, do: :xpto
   def foobar, do: :almost_done
+  def foo2, do: :foo
+  def bar2, do: :bar
+  def xpto2, do: :xpto
 end
