c21d0d0da82605676e0a5e5b0450bfd2c6eac59a
Mário Guimarães <mario.luis.guimaraes@gmail.com>
2020-10-07 11:10:10 +0100

    Modified zz.ex a lot

--- lib/zz.ex
+++ lib/zz.ex
@@ -1,4 +1,14 @@
 defmodule Z do
+  def ba(), do: :ba
+  def be(), do: :be
+  def bi(), do: :bi
+  def bo(), do: :bo
+  def bu(), do: :bu
+  def da(), do: :da
+  def de(), do: :de
+  def di(), do: :di
+  def do(), do: :do
+  def du(), do: :du
 end
 
 # this is a comment
